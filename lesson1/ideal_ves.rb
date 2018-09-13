23ff1985c2b39237e13c86608107401d992a0534
celtic93 <egorka80@list.ru>
2018-09-13 20:53:38 +0300

    Fixed otstupy

--- lesson1/ideal_ves.rb
+++ lesson1/ideal_ves.rb
@@ -7,7 +7,7 @@ rost = gets.to_i
 ideal = rost - 110
 
 if ideal > 0
-	puts "#{name}, Ваш идеальный вес - #{ideal} кг."
+  puts "#{name}, Ваш идеальный вес - #{ideal} кг."
 else
-	puts 'Ваш вес оптимальный.'
+  puts 'Ваш вес оптимальный.'
 end
