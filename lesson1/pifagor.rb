db5940ed6b089c372e0f31a2e1224a221dcbb03f
celtic93 <egorka80@list.ru>
2018-09-13 20:33:10 +0300

    Fixed otstup

--- lesson1/pifagor.rb
+++ lesson1/pifagor.rb
@@ -3,6 +3,7 @@ a = gets.to_i
 b = gets.to_i
 c = gets.to_i
 
+#строка
 if a**2 == b**2 + c**2 || b**2 == c**2 + a**2 || c**2 == a**2 + b**2
 	puts 'Треугольник прямоугольный'
 elsif a == b && b == c 	 
