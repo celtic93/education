013c64add137b210a390a69613729860ebfc7a47
celtic93 <egorka80@list.ru>
2018-09-14 19:55:20 +0300

    Lesson 2

---
+++ lesson2/1months.rb
@@ -0,0 +1,20 @@
+months = {
+  'Jan' => 31,
+  'Feb' => 28,
+  'Mar' => 31,
+  'Apr' => 30,
+  'May' => 31,
+  'Jun' => 30,
+  'Jul' => 31,
+  'Aug' => 31,
+  'Sep' => 30,
+  'Oct' => 31,
+  'Nov' => 30,
+  'Dec' => 31
+}
+
+months.to_a.each do |month|
+  if month[1] == 30
+    puts month[0]
+  end
+end
